d31744630a59dc1f2df87f8e1c565d16dd106ad4
Robert Guss <robert@vmi3369320.contaboserver.net>
2026-06-17 21:12:56 +0200

    feat: add telemetry conventions (software-factory-ai-q19.2.4)

--- lib/conveyor/telemetry.ex
+++ lib/conveyor/telemetry.ex
@@ -1,4 +1,16 @@
 defmodule Conveyor.Telemetry do
   @moduledoc "Conductor trace, metric, and log emission service skeleton."
   use Conveyor.Conductor.Child
+
+  alias Conveyor.Telemetry.Conventions
+
+  @spec emit_metric([atom()], map(), map()) ::
+          :ok | {:error, {:disallowed_metric_dimensions, [String.t()]}}
+  def emit_metric(event_name, measurements, metadata \\ %{})
+      when is_list(event_name) and is_map(measurements) and is_map(metadata) do
+    with :ok <- Conventions.validate_metric_dimensions(metadata) do
+      :telemetry.execute(event_name, measurements, metadata)
+      :ok
+    end
+  end
 end
